1df22001f5e584fe755746deb9d03af664ab4134
fischer23 <88708145+fischer23@users.noreply.github.com>
2025-05-08 13:53:23 +0200

    Update generate_plots.R

--- generate_plots.R
+++ generate_plots.R
@@ -63,12 +63,11 @@ ggsave("results/plot_KR.pdf", plot=plot_KR, width=12, height=8)
 
 #Creates Figure 6 of the paper "Admissible online closed testing must employ e-values"
 
-df_GRO=TDP_df[TDP_df$Procedure %in% c("GRO", "hedged GRO", "boosted GRO", "true proportion"), ]
-col_GRO=c("darksalmon", "orange", "red", "grey")
+df_GRO=TDP_df[TDP_df$Procedure %in% c("GRO", "hedged GRO", "true proportion"), ]
+col_GRO=c("darksalmon", "orange", "grey")
 
 line_types = c("GRO" = "dotdash", 
                "hedged GRO" = "dashed", 
-               "boosted GRO" = "dotted", 
                "true proportion" = "solid")
 
 plot_GRO=ggplot(df_GRO, aes(x=index, y=TDP, colour = Procedure, linetype = Procedure)) + 
@@ -92,12 +91,11 @@ ggsave("results/plot_GRO.pdf", plot=plot_GRO, width=12, height=8)
 
 #Creates Figure 7 of the paper "Admissible online closed testing must employ e-values"
 
-df_best=TDP_df[TDP_df$Procedure %in% c("admissible online-simple", "boosted GRO", "calibrated", "true proportion"), ]
-col_best=c("cornflowerblue", "red", "limegreen", "grey")
+df_best=TDP_df[TDP_df$Procedure %in% c("admissible online-simple", "hedged GRO", "true proportion"), ]
+col_best=c("cornflowerblue", "orange", "grey")
 
-line_types = c("calibrated" = "dotdash", 
-               "admissible online-simple" = "dashed", 
-               "boosted GRO" = "dotted", 
+line_types = c("admissible online-simple" = "dotted", 
+               "hedged GRO" = "dashed", 
                "true proportion" = "solid")
 
 plot_best=ggplot(df_best, aes(x=index, y=TDP, colour = Procedure, linetype = Procedure)) + 
@@ -171,3 +169,31 @@ plot_Freed=ggplot(df_Freed, aes(x=index, y=TDP, colour = Procedure, linetype = P
         strip.text.y = element_text(size = 15))
 
 ggsave("results/plot_m-Freed.pdf", plot=plot_Freed, width=12, height=8)
+
+#Creates Figure S.3 of the paper "Admissible online closed testing must employ e-values"
+
+df_boost=TDP_df[TDP_df$Procedure %in% c("admissible online-simple", "boosted GRO", "calibrated", "true proportion"), ]
+col_boost=c("cornflowerblue", "orange", "limegreen", "grey")
+
+line_types = c("admissible online-simple" = "dotted",
+               "boosted GRO" = "dashed", 
+               "calibrated" = "dotdash", 
+               "true proportion" = "solid")
+
+plot_boost=ggplot(df_boost, aes(x=index, y=TDP, colour = Procedure, linetype = Procedure)) + 
+  geom_line(size = 1.2, aes(colour = Procedure)) +
+  facet_grid(mu_A ~ pi_A)+
+  scale_colour_manual(values=col_boost)+
+  scale_linetype_manual(values = line_types)+
+  xlab("Hypothesis index")+
+  ylab("TDP bound")+
+  scale_x_continuous(breaks = seq(100, 900, 200), limits=c(0, 1000), expand = c(0, 0)) +
+  scale_y_continuous(breaks = seq(0.1, 0.9, 0.2), limits=c(0, 1), expand = c(0.02, 0)) +
+  theme(panel.background = element_blank(),panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        panel.border = element_rect(colour = "black", fill=NA, size=1), 
+        axis.title.y = element_text(size=20), axis.title.x = element_text(size=20),
+        legend.text=element_text(size=20), legend.title=element_text(size=20), strip.text.x = element_text(size = 15),
+        strip.text.y = element_text(size = 15))
+
+ggsave("results/plot_boost.pdf", plot=plot_boost, width=12, height=8)
